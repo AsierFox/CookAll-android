c8b3e896f08259337bea749d28f5c0865f84d302
asierg <asigonlo@gmail.com>
2017-10-18 16:30:14 +0200

    Move public constant to Constants

--- app/src/main/java/com/devdream/cookall/core/api/APIRestClient.java
+++ app/src/main/java/com/devdream/cookall/core/api/APIRestClient.java
@@ -1,18 +1,18 @@
 package com.devdream.cookall.core.api;
 
+import com.devdream.cookall.core.utils.Constants;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIRestClient {
-
-    private static final String API_BASE_URL = "http://cookall.devdream.eu";
 
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(API_BASE_URL)
+                    .baseUrl(Constants.API_BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
--- app/src/main/java/com/devdream/cookall/core/utils/Constants.java
+++ app/src/main/java/com/devdream/cookall/core/utils/Constants.java
@@ -4,4 +4,6 @@ public abstract class Constants {
 
     public static final String APP_NAME = "CookAll";
 
+    public static final String API_BASE_URL = "http://cookall.devdream.eu";
+
 }
