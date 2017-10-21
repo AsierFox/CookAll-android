724c19b2a1b1f1dbf4557a3a39a58227dc8cfc1f
Asier González López <asigonlo@gmail.com>
2017-10-21 15:25:07 +0200

    Integrate Notification intent service & custom notifications

--- app/src/main/java/com/devdream/cookall/test/TestActivity.java
+++ app/src/main/java/com/devdream/cookall/test/TestActivity.java
@@ -5,17 +5,15 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.RemoteViews;
 
 import com.devdream.cookall.R;
 
 public class TestActivity extends AppCompatActivity {
-
-    private CharSequence notificationTitleText = "Hello Title";
-    private CharSequence notificationDescText = "This is custom Notification desc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +28,16 @@ public class TestActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TestActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(TestActivity.this, 0, intent, 0);
 
-        // Inflating our custom layout by the RemoteViews class
-        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_custom_test);
-
-        // Setting custom layout views properties
-//        remoteViews.setImageViewResource(R.id.imageViewIcon, R.drawable.ic_liked_24dp);
-        remoteViews.setTextViewText(R.id.textViewTitle, notificationTitleText);
-        remoteViews.setTextViewText(R.id.textViewDesc, notificationDescText);
-
         // Create a notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
         // Setting small icon for our notification
         builder.setSmallIcon(R.drawable.app_logo);
-
-        // Attaching our notification
         builder.setContentTitle("My notification");
         builder.setContentText("Description of the notification");
 
-
         // ATTACHING PENDING INTENT TO NOTIFICATION
+        builder.setAutoCancel(true);
         builder.setContentIntent(pendingIntent);
 
         // And finally NotificationManager for the notify the user!
@@ -59,6 +47,47 @@ public class TestActivity extends AppCompatActivity {
     }
 
     public void testCustomNotification(View view) {
+        Intent intent = new Intent(this, TestActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(TestActivity.this, 0, intent, 0);
+
+        RemoteViews collapsedView = new RemoteViews(getPackageName(), R.layout.notification_custom_test);
+        RemoteViews expandedView = new RemoteViews(getPackageName(), R.layout.notification_custom_test_expanded);
+
+        expandedView.setImageViewResource(R.id.big_icon, R.drawable.app_logo);
+        expandedView.setTextViewText(R.id.timestamp, DateUtils.formatDateTime(this, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME));
+        expandedView.setTextViewText(R.id.notification_message, "Test");
+
+        collapsedView.setImageViewResource(R.id.big_icon, R.drawable.app_logo);
+        collapsedView.setTextViewText(R.id.timestamp, DateUtils.formatDateTime(this, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME));
+
+        // adding action to left button
+        Intent leftIntent = new Intent(this, NotificationIntentService.class);
+        leftIntent.setAction("left");
+        expandedView.setOnClickPendingIntent(R.id.left_button, PendingIntent.getService(this, 0, leftIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+        // adding action to right button
+        Intent rightIntent = new Intent(this, NotificationIntentService.class);
+        rightIntent.setAction("right");
+        expandedView.setOnClickPendingIntent(R.id.right_button, PendingIntent.getService(this, 1, rightIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                // these are the three things a NotificationCompat.Builder object requires at a minimum
+                .setSmallIcon(R.drawable.app_logo)
+                .setContentTitle("Test")
+                .setContentText("test")
+                // notification will be dismissed when tapped
+                .setAutoCancel(true)
+                // tapping notification will open MainActivity
+                .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, TestActivity.class), 0))
+                // setting the custom collapsed and expanded views
+                .setCustomContentView(collapsedView)
+                .setCustomBigContentView(expandedView)
+                // setting style to DecoratedCustomViewStyle() is necessary for custom views to display
+                .setStyle(new android.support.v7.app.NotificationCompat.DecoratedCustomViewStyle());
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        final int NOTIFICATION_ID = 2;
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
 }
